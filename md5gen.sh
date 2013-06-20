7ab15bce1969ac5c10dda3b15dbe9f316d0efe50
Alex <alex@gmail.com>
2013-06-20 11:53:41 -0700

    fix

--- md5gen.sh
+++ md5gen.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd $1;find . -type f | while read line;do md5sum $line;done > md5sums
+cd $1;rm master.md5 md5sums;find . -type f | while read line;do md5sum $line;done > md5sums
 md5sum md5sums > master.md5
